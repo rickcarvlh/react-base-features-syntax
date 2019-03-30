33225bfd78525f9189dd9487ed5501b2c95444f9
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-30 15:50:25 +0000

    example of hooks finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
@@ -16,39 +16,42 @@ import Person from './Person/Person';
 // React hooks Example
 const app = props => {
 
+    const [personsState, setPersonsState] = useState({
+        persons: [
+            {name: 'Max', age: 28},
+            {name: 'Manu', age: 29},
+            {name: 'Stephanie', age: 26},
+        ],
+        otherState: 'some other value'
+    });
+
+    // note it became in function inside a function
+    const switchNameHandler = () => {
+        // console.log('was clicked');
+        setPersonsState({
+            persons: [
+                {name: 'Maximilian', age: 28},
+                {name: 'Manu', age: 29},
+                {name: 'Stephanie', age: 27},
+            ],
+        });
+    };
+
+    // note -> personState replaces persons array in the return
     return (
-      <div className="App">
-       <h1>Hi I'm a react app</h1>
-       <p>This is real working</p>
-       <button onClick={this.switchNameHandler}>Switch Name</button>
-       <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name}>My Hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name}/>
-      </div>
+        <div className="App">
+            <h1>Hi I'm a react app</h1>
+            <p>This is real working</p>
+            <button onClick={switchNameHandler}>Switch Name</button>
+            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+            <Person name={personsState.persons[1].name}>My Hobbies: Racing</Person>
+            <Person name={personsState.persons[2].name}/>
+        </div>
     );
+
 };
-
-
 export default app;
 
-// state = {
-//     persons: [
-//         { name: 'Max', age: 28 },
-//         { name: 'Manu', age: 29 },
-//         { name: 'Stephanie', age: 26 },
-//     ],
-//     otherState: 'some other value'
-// };
-//
-// //  chaning name using state
-// switchNameHandler = () => {
-//     // console.log('was clicked');
-//     this.setState({
-//         persons: [
-//             { name: 'Maximilian', age: 28 },
-//             { name: 'Manu', age: 29 },
-//             { name: 'Stephanie', age: 27 },
-//         ],
-//     })
-//
+
+
 
