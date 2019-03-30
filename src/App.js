cf95a5420811af9a0305250ec04b579bc2c2270d
Ricardo Carvalho <ricardo.carvlh84@gmail.com>
2019-03-30 17:56:26 +0000

    App js returned to a class Component no hooks

--- src/App.js
+++ src/App.js
@@ -1,62 +1,53 @@
-import React, {useState} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-// class App extends Component {
-//   state = {
-//     persons: [
-//       {name: 'Max', age: 28},
-//       {name: 'Manu', age: 29},
-//       {name: 'Stephanie', age: 26},
-//     ],
-//     otherState: 'some other value'
-//   }
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
 
-// TODO example for React hooks
-// React hooks Example
-const app = props => {
-
-    const [personsState, setPersonsState] = useState({
-        persons: [
-            {name: 'Max', age: 28},
-            {name: 'Manu', age: 29},
-            {name: 'Stephanie', age: 26},
-        ],
-
+  switchNameHandler = () => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
     });
+  };
 
-    // manage our other state
-    const [otherState, setOtherState] =  useState('some other value');
-
-    console.log(otherState, setOtherState );
-
-    // note it became in function inside a function
-    const switchNameHandler = () => {
-        // console.log('was clicked');
-        setPersonsState({
-            persons: [
-                {name: 'Maximilian', age: 28},
-                {name: 'Manu', age: 29},
-                {name: 'Stephanie', age: 27},
-            ],
-        });
-    };
-
-    // note -> personState replaces persons array in the return
+  render() {
     return (
-        <div className="App">
-            <h1>Hi I'm a react app</h1>
-            <p>This is real working</p>
-            <button onClick={switchNameHandler}>Switch Name</button>
-            <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-            <Person name={personsState.persons[1].name}>My Hobbies: Racing</Person>
-            <Person name={personsState.persons[2].name}/>
-        </div>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My Hobbies: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
 
-};
-export default app;
-
-
-
-
+export default App;
